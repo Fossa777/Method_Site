d6d0105eebb0edc72d629484059c0c4413c6d648
Fossa777 <158113741+Fossa777@users.noreply.github.com>
2025-05-08 23:20:13 +1000

    Update template-loader.js

--- assets/js/template-loader.js
+++ assets/js/template-loader.js
@@ -1,1 +1,17 @@
+async function loadHTML(id, file) {
+  const el = document.getElementById(id);
+  if (el) {
+    const response = await fetch(file);
+    if (response.ok) {
+      const text = await response.text();
+      el.innerHTML = text;
+    } else {
+      el.innerHTML = "<p>Ошибка загрузки " + file + "</p>";
+    }
+  }
+}
 
+document.addEventListener("DOMContentLoaded", () => {
+  loadHTML("header", "/header.html");
+  loadHTML("footer", "/footer.html");
+});
