1a7ba274153c97afe931264cd68c7efba3687227
Fossa777 <158113741+Fossa777@users.noreply.github.com>
2025-05-08 23:22:01 +1000

    Update gallery.js

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,1 +1,14 @@
+const images = [
+  "/assets/images/gallery/image1.jpg",
+  "/assets/images/gallery/image2.jpg"
+];
 
+const gallery = document.getElementById("gallery");
+
+images.forEach(src => {
+  const img = document.createElement("img");
+  img.src = src;
+  img.alt = "Изображение галереи";
+  img.className = "gallery-image";
+  gallery.appendChild(img);
+});
